cf89edd0fa52251700ba3abd41d1199ce21fae54
prateekraiger <prateekraiger123@gmail.com>
2026-04-22 19:44:40 +0530

    c3

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Spylt-awward-clone/" : "/",
+  base: "/",
   build: {
-    outDir: "docs"
+    outDir: "dist"
   },
   plugins: [react(), tailwindcss()],
 });
